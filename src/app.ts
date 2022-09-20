b81f3d7ab4ac0e45f2cb3930e3c9eaeb0ae21870
Dener Rocha <denernun@gmail.com>
2022-09-20 16:46:43 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
-class App {
-  static start(): void {
-    console.log('Application Start');
-    // AbstractFactoryPattern.Demo.show();
-  }
-}
+import { Subject } from 'rxjs';
 
-App.start();
+const subject = new Subject<number>();
+subject.subscribe((val: number) => console.log(val));
+subject.next(10);
+subject.next(20);
+
+// class App {
+//   static start(): void {
+//     console.log('Application Start');
+//   }
+// }
+// App.start();
