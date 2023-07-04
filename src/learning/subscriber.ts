e8980e9f8b0848619d6e8a3b47c7a551f7e1aa29
denernun <denernun@gmail.com>
2023-07-04 19:25:36 -0300

    .

--- src/learning/subscriber.ts
+++
@@ -1,8 +0,0 @@
-import { Subject } from 'rxjs';
-
-export const execute = (): void => {
-  const subject = new Subject<number>();
-  subject.subscribe((val: number) => console.log(val));
-  subject.next(10);
-  subject.next(20);
-};
--- src/learning/types/const-type.ts
+++
@@ -1,7 +0,0 @@
-namespace ConstType {
-  const obj = {
-    name: 'John',
-    age: 30,
-  } as const;
-  console.log(obj.name);
-}
